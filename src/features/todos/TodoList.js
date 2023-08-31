25d95e400f9c2d32ba62eb8db63ec3ff93a1ea13
Abhilash <abhilashjha335@gmail.com>
2023-08-31 14:52:50 +0530

    React-Query project

---
+++ src/features/todos/TodoList.js
@@ -0,0 +1,100 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+
+import { getTodos, addTodo, updateTodo, deleteTodo  } from "../../api/todosApi"
+
+import { useState } from "react"
+
+const TodoList = () => {
+    const [newTodo, setNewTodo] = useState('');
+    const queryClient = useQueryClient();
+
+    const {
+        isLoading, 
+        isError,
+        error,
+        data : todos
+    } = useQuery(['todos'], getTodos, {
+        select: data => data.sort((a,b) => b.id - a.id)
+    });
+
+    const addTodoMutation = useMutation(addTodo, {
+        onSuccess: () => {
+            // Invalidates cache and refetch
+            queryClient.invalidateQueries('todos');
+        }
+    })
+
+    const updateTodoMutation = useMutation(updateTodo, {
+        onSuccess: () => {
+            // Invalidates cache and refetch
+            queryClient.invalidateQueries('todos');
+        }
+    })
+
+    const deleteTodoMutation = useMutation(deleteTodo, {
+        onSuccess: () => {
+            // Invalidates cache and refetch
+            queryClient.invalidateQueries('todos');
+        }
+    })
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        addTodoMutation.mutate({userId: 1, title: newTodo, completed: false});
+        setNewTodo('');
+    }
+
+    const newItemSection =  (
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="new-todo">Enter a new todo item</label>
+            <div className="new-todo">
+                <input type="text" id="new-todo"
+                  value={newTodo}
+                  onChange={(e)=> setNewTodo(e.target.value)}
+                  placeholder="Enter new Todo"
+                />
+            </div>
+
+            <button className="submit">Add</button>
+
+        </form>
+        )
+ 
+
+    let content;
+    if(isLoading){
+       content = <p>Loading...</p>
+    }
+    else if(isError){
+        content = <p>{error.message}</p>
+    }
+    else{
+        content = todos.map((todo) => {
+            return (
+                <article key={todo.id}>
+                    <div className="todo">
+                        <input type="checkbox" id={todo.id}
+                          checked={todo.completed}
+                          onChange={()=> updateTodoMutation.mutate({...todo, completed: !todo.completed})}
+                        />
+                        <label htmlFor={todo.id}>{todo.title}</label>
+                    </div>
+                    <button className="trash"
+                      onClick={()=> deleteTodoMutation.mutate({id: todo.id})}
+                    >
+                        Delete
+                    </button>
+                </article>
+            )
+        })
+    }
+  return (
+     <main>
+        <h1>Todo List</h1>
+         {newItemSection}
+         {content}
+     </main>
+  )
+}
+
+export default TodoList
